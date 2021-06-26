0eda1c9a7bf5f1664a7cfcf643cb874023a655d9
CRPMD <clean.renov.plus.contact@gmail.com>
2021-06-26 07:56:39 +0100

    cors

--- add-ids.js
+++ add-ids.js
@@ -2,6 +2,25 @@
 
 const fs = require("fs");
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 const add_id = async (_req_, _res_) => {
   try {
     if (!_req_.body.id || !_req_.body.title) {
@@ -27,4 +46,4 @@ const add_id = async (_req_, _res_) => {
   }
 };
 
-module.exports.add_id = add_id;
+module.exports.add_id = allowCors(add_id);
--- login.js
+++ login.js
@@ -1,4 +1,23 @@
 const fs = require("fs");
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 const login = async (_req_, _res_) => {
   let user = await fs.readFileSync("admin.text");
   if (_req_.body.userID === user) {
@@ -8,4 +27,4 @@ const login = async (_req_, _res_) => {
   }
 };
 
-module.exports.login = login;
+module.exports.login = allowCors(login);
