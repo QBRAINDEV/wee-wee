e728512f0a42ead90c53cdf2b2870f23bc6d63ed
CRPMD <clean.renov.plus.contact@gmail.com>
2021-06-28 17:35:31 +0100

    index

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log(`serving wee-wee-server`);
 });
