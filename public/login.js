1bcda2fdac8e04d8a667e51bb3aac9ca8ba4c9b3
CRPMD <clean.renov.plus.contact@gmail.com>
2021-06-28 17:33:10 +0100

    index

--- public/login.js
+++ public/login.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
-
+const admin = require(`../admin.json`);
 const login = async (_req_, _res_) => {
-  let user = await fs.readFileSync("admin.text");
-  if (_req_.body.userID === user) {
+  let login = admin.login;
+  login = admin.login;
+  if (_req_.body.userID === login) {
     _res_.json({ user: true });
   } else {
     _res_.json({ error: true });
