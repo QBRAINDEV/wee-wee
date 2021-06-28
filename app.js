f101d61f164cf8e73733713d9697911e418234c1
CRPMD <clean.renov.plus.contact@gmail.com>
2021-06-28 17:40:09 +0100

    index

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(__dirname + "/public/css"));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(8080, () => {
