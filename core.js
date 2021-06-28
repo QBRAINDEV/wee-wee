22682219bc2b4a5368f46ab3a95b6300e181adc0
CRPMD <clean.renov.plus.contact@gmail.com>
2021-06-28 16:39:20 +0100

    f & b

--- core.js
+++ core.js
@@ -17,6 +17,22 @@ class Core {
     this.__app.use(express.json());
     this.__app.use(helmet());
 
+    this.__app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+      );
+      if (req.method === "OPTIONS") {
+        res.header(
+          "Access-Control-Allow-Methods",
+          "POST, PUT, PATCH, GET, DELETE"
+        );
+        return res.status(200).json({});
+      }
+      next();
+    });
+
     this.__resources__();
   }
 
--- get-videos-ids.js
+++ get-videos-ids.js
@@ -1,28 +1,10 @@
 "use strict";
 
 const fs = require("fs");
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+
 let ids;
 const add = async () => {
   ids = await fs.readFileSync("videos-ids.json");
   return JSON.parse(ids);
 };
-module.exports.ids = allowCors(add);
+module.exports.ids = add;
